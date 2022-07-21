c35e12ca18e4134e7fe7d7cb90591688a9151ae4
Nazareno <nazareno.anselmi@gmail.com>
2022-07-21 17:06:35 -0300

    change def. route

--- vpc.tf
+++ vpc.tf
@@ -83,8 +83,9 @@ resource "aws_network_acl" "default_nacl" {
 
 resource "aws_route_table" "private_backend_crt_apprendiendo" {
   route {
-    cidr_block           = "0.0.0.0/0"
-    network_interface_id = aws_network_interface.apprendiendo_mikrotik_eni.id
+    cidr_block  = "0.0.0.0/0"
+    instance_id = aws_instance.apprendiendo_mikrotik.id
+    #network_interface_id = aws_network_interface.apprendiendo_mikrotik_eni.id
   }
   vpc_id = aws_vpc.apprendiendo_vpc.id
   tags = {
