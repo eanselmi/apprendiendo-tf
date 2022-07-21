dcce978b1074e33ca4650c60acea25e90ed449a7
Nazareno <nazareno.anselmi@gmail.com>
2022-07-21 10:06:42 -0300

    fix typo

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "apprendiendo_vpc " {
+resource "aws_vpc" "apprendiendo_vpc" {
   assign_generated_ipv6_cidr_block = "false"
   cidr_block                       = "10.0.0.0/16"
   enable_classiclink               = "false"
